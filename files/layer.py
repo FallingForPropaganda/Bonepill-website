4c890342fc162b50c2d4b2c8eeaac67ff4372f9e
MichaelTorry <fakeemail5016@gmail.com>
2023-02-28 14:16:05 -0500

    fixed the bonepill i think

--- files/layer.py
+++ files/layer.py
@@ -2,7 +2,8 @@ import tt
 def layer(mx, my, x, y, g, u):
       import tt
                                         
-      
+      g = g.lower()
+      u = u.lower()
       fig = tt.grapher(g, u, x, y, mx, my)
       fig.set_figheight(6) 
       fig.set_figwidth(10.5)  
--- files/test.py
+++ files/test.py
@@ -5,7 +5,7 @@ genders_to_show = 'mf'
 units = 'in'                                     
 input_x = 37.5
 input_y = 27.5
-measurement_x = 'Buttocks Circumference'
+measurement_x = 'height'
 measurement_y = 'Waist Circumference'
 
 fig = tt.grapher(genders_to_show, units, input_x, input_y, measurement_x, measurement_y)
